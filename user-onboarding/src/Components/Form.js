6912db24a17417d81b9ae94960ce1e278c89ed45
newbi462 <isthisemailfree@gmail.com>
2019-11-14 16:03:05 -0500

    STEP 1 - Create Your Formik Form

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -3,20 +3,33 @@ import React, { useState, useEffect } from "react";
 import {withFormik, Form, Field } from "formik";
 
 
-const PreFormikForm = props => {
+const PreFormikForm = ( { values } ) => {
 
 
   return (
     <Form>
       <Field type="text" name="name" placeholder="Your Name" />
+      <Field type="text" name="email" placeholder="E-Mail" />
+      <Field type="password" name="password" placeholder="Password" />
+
+      <Field
+        type="checkbox"
+        name="tos"
+        checked={values.tos}
+      />
+
+      <button>Submit :)</button>
     </Form>
   );
 };
 
 const FormikForm = withFormik({
-  mapPropsToValues({ name }) {
+  mapPropsToValues({ name, email, password, tos }) {
     return {
       name: name || "",
+      email: email || "",
+      password: password || "",
+      tos: tos || false,
     };
   },
 })(PreFormikForm);
