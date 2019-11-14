efb24404f18062538c161ac5c9dbff4679fcd060
newbi462 <isthisemailfree@gmail.com>
2019-11-14 16:35:49 -0500

    STEP 2 - Implement Form Validation and Error Messaging

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from "react";
 
 import {withFormik, Form, Field } from "formik";
+import * as Yup from "yup"; // for everything
 
 
-const PreFormikForm = ( { values } ) => {
-
+const PreFormikForm = ( { values, errors, touched } ) => {
 
   return (
     <Form>
       <Field type="text" name="name" placeholder="Your Name" />
+      {touched.name && errors.name && (
+        <p className="errors">{errors.name}</p>
+      )}
+
       <Field type="text" name="email" placeholder="E-Mail" />
+      {touched.email && errors.email && (
+        <p className="errors">{errors.email}</p>
+      )}
+
       <Field type="password" name="password" placeholder="Password" />
 
-      <Field
-        type="checkbox"
-        name="tos"
-        checked={values.tos}
-      />
+      <label className="checkbox-container">
+        Check to agree to the TOS
+        <Field
+          type="checkbox"
+          name="tos"
+          checked={values.tos}
+        />
+      </label>
+      {touched.tos && errors.tos && (
+        <p className="errors">{errors.tos}</p>
+      )}
+
 
       <button>Submit :)</button>
     </Form>
@@ -32,5 +47,10 @@ const FormikForm = withFormik({
       tos: tos || false,
     };
   },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().required(),
+    tos: Yup.boolean(true).required()
+  }),
 })(PreFormikForm);
 export default FormikForm;
